924e43928e135ce38f1fb6de62e7fd06eac6131d
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-09-28 21:47:57 -0300

    MainPage implemented

---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,27 @@
+import React, { useState, } from 'react';
+import { MdSearch } from 'react-icons/md'
+import { Container, Logo, Title, Form, Input, Button } from './styles';
+import githubLogo from '../../assets/images/github-logo.svg';
+
+const MainPage = () => {
+  const [login, setLogin] = useState('');
+
+  return (
+    <Container>
+      <Logo src={githubLogo} alt="Logo github" />
+      <Title>API Github</Title>
+      <Form>
+        <Input
+          placeholder="usuário"
+          value={login}
+          onChange={(event) => setLogin(event.target.value)}
+        />
+        <Button to={`${login}/repositories`}>
+          <MdSearch size={42} />
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default MainPage;
